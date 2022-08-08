a0eda1ed9be65e65b567008511bae5593936b6da
Timur <timur.klenner-yilmaz@dertouristik.com>
2022-08-08 10:07:52 +0200

    feat: add fleet table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCars } from "./api/cars";
+import CarList from "./components/List/CarList";
+import { Car } from "./types";
 
 function App() {
+  const [cars, setCars] = useState<Car[]>([]);
+
+  useEffect(() => {
+    const getCarsData = async () => {
+      const cars = await getCars();
+      setCars(cars);
+    };
+    getCarsData();
+  }, []);
+
   return (
-    <div className="App">
-      <div>inital setup</div>
+    <div className="mx-10">
+      <h1 className="text-3xl font-bold my-10 text-center">Fleet</h1>
+      <CarList cars={cars} />
     </div>
   );
 }
